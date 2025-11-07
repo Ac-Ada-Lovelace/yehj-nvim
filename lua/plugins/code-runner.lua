639b88304421881931132b4e70dc4e4e639436d2
yehj <ac1583001@gmail.com>
2025-11-07 22:28:59 +0800

    Initial commit

---
+++ lua/plugins/code-runner.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "CRAG666/code_runner.nvim",
+    config = function()
+      require("code_runner").setup({
+        filetype = {
+          python = "python3 -u",
+          javascript = "node",
+          typescript = "ts-node",
+          rust = "cargo run",
+          go = "go run",
+          c = "gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+          cpp = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+        },
+      })
+    end,
+    keys = {
+      { "<leader>jr", ":RunCode<CR>", desc = "Run code" },
+      { "<leader>jf", ":RunFile<CR>", desc = "Run file" },
+      { "<leader>jp", ":RunProject<CR>", desc = "Run project" },
+    },
+  },
+
+  {
+    "michaelb/sniprun",
+    build = "bash ./install.sh",
+    config = function()
+      require("sniprun").setup({
+        display = { "Classic", "VirtualTextOk" },
+      })
+    end,
+    keys = {
+      { "<leader>jR", ":'<,'>SnipRun<CR>", mode = "v", desc = "Run selection" },
+      { "<leader>jc", ":SnipRun<CR>", desc = "Run current file" },
+    },
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup()
+    end,
+    keys = {
+      { "<leader>jt", "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
+    },
+  },
+}
